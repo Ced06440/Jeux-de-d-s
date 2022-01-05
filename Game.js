7a9cd8d3142e849e262834fafdacf68c19245c1f
Cedric Bernier <bernier.cedric_06@hotmail.fr>
2022-01-05 18:10:16 +0100

    Premier Commit

--- Game.js
+++ Game.js
@@ -1,21 +1,21 @@
-var score , roundScore, gamePlaying, player;
+var score, gamePlaying, roundScore;
+var activePlayer = document.querySelector('.active');
 
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
-    if(gamePlaying){
-
+document.querySelector('.lancer').addEventListener('click', function() {
+    
         //Numeros dés au hasard
-        var dice = Math.floor(Math.random()*6)+1
+        var dice = Math.floor(Math.random()*6)+1;
 
         //Affichage face du dés
         var diceDOM = document.querySelector('.dés');
-        diceDOM.style.display = 'block';
         diceDOM.src = 'dés-' + dice + '.png';
 
-        if(dice !== 1){
+        //Ajout de la régle, si le dés tombe sur 1 c'est au tour du joueur suivant
+        if (dice !== 1) {
             roundScore += dice;
+            document.querySelector('#score').textContent = roundScore;
         } else {
             nextPlayer();
-        }
-    }
+        }  
 });
