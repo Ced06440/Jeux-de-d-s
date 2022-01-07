bafc4c172c12c3c7fea0c0299523882229d1ef50
Cedric Bernier <bernier.cedric_06@hotmail.fr>
2022-01-07 09:02:13 +0100

    ajout du bouton ajouter

--- Game.js
+++ Game.js
@@ -1,5 +1,8 @@
-var score, gamePlaying, roundScore;
-var activePlayer = document.querySelector('.active');
+let score = document.querySelector('.scoreGlobal-0');
+let roundScore = document.getElementById('#score-0');
+let gamePlaying = false;
+let player1 = document.querySelector('.player-0');
+let player2 = document.querySelector('.player-1');
 
 
 document.querySelector('.lancer').addEventListener('click', function() {
@@ -13,9 +16,25 @@ document.querySelector('.lancer').addEventListener('click', function() {
 
         //Ajout de la régle, si le dés tombe sur 1 c'est au tour du joueur suivant
         if (dice !== 1) {
-            roundScore += dice;
-            document.querySelector('#score').textContent = roundScore;
+            document.querySelector('#score-0').textContent = roundScore += dice;
         } else {
+            document.querySelector('#score-0').textContent =roundScore = 0;
             nextPlayer();
         }  
+});
+
+document.querySelector('.ajouter').addEventListener('click', function() {
+
+    let scoreG = roundScore + score;
+    document.querySelector('#scoreGlobal-0').textContent = scoreG;
+
+    document.querySelector('#score-0').textContent=0
+
+    if(scoreG >= 100){
+        document.querySelector('player-0').classList.add('winner');
+        document.querySelector('player-0').classList.remove('active')
+    }else{
+        nextPlayer()
+    }
+
 });
